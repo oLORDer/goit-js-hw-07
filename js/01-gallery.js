126934d39cdf10b2929848da20ba4a187ca50bdf
oLORDer <justsendmeow@gmail.com>
2022-07-03 01:19:02 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
 // Change code below this line
 
-console.log(galleryItems);
+// const galleryItem = galleryItems.map(item =>
+//     `<div class="gallery__item">
+//     <a class="gallery__link" href=${item.original}>
+//       <img
+//         class="gallery__image"
+//         src=${item.preview}
+//         data-source=${item.original}
+//         alt=${item.description}
+//       />
+//     </a>
+//   </div>`
+//   );
+
+console.log(basicLightbox);
+
+const instance = basicLightbox.create(`<img src="assets/images/image.png" width="800" height="600">`);
+
+instance.show();
+
+// document.querySelector('.gallery').insertAdjacentHTML('afterend', galleryItem.join(''));
+
+// console.log(galleryItem);
